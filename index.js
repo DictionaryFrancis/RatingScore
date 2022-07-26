6791d6a75f0d01ff5ec90b73eb2185a945870086
Francis Matias <fwmatias64@gmail.com>
2022-07-26 16:00:28 +0100

    Add button Event
    Button clicked and change color in JS component

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-rating = 0
+//---Buttons Clicked Color---//
+let btn1Color = document.querySelector("#btn-1")
+let btn2Color = document.querySelector("#btn-2")
+let btn3Color = document.querySelector("#btn-3")
+let btn4Color = document.querySelector("#btn-4")
+let btn5Color = document.querySelector("#btn-5")
 
-let getRating = () => {
-    return rating
-}
-let setRating = (picked) => {
-    rating = picked
-}
 
-let submitRating = () => {
-    if (rating ===0) {
-        document.
-    }
-}
+btn1Color.addEventListener("click", function(){
+    
+    btn1Color.classList.add("clicked")
+})
+btn2Color.addEventListener("click", function(){
+    btn2Color.classList.add("clicked")
+})
+btn3Color.addEventListener("click", function(){
+    btn3Color.classList.add("clicked")
+})
+btn4Color.addEventListener("click", function(){
+    btn4Color.classList.add("clicked")
+})
+btn5Color.addEventListener("click", function(){
+    btn5Color.classList.add("clicked")
+})
+
+//-------------------Submit
+
+
+
+
+// let submit = document.querySelector("#submit-btn")
+// submit.addEventListener("click",function(){
+
+// })
+
+
+
+
+
+
+//rating = 0
+
+// let getRating = () => {
+//     return rating
+// }
+// let setRating = (picked) => {
+//     rating = picked
+// }
+
+// let submitRating = () => {
+//     if (rating ===0) {
+//         document.
+//     }
