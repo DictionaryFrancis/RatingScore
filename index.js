4d7eb36ead0437f1c7eded48e9b548e2d99a7109
Francis Matias <fwmatias64@gmail.com>
2022-07-26 17:04:01 +0100

    Adding Submit button

--- index.js
+++ index.js
@@ -3,29 +3,45 @@ let btn1Color = document.querySelector("#btn-1")
 let btn2Color = document.querySelector("#btn-2")
 let btn3Color = document.querySelector("#btn-3")
 let btn4Color = document.querySelector("#btn-4")
-let btn5Color = document.querySelector("#btn-5")
+let btn5Color = document.querySelector("#btn-5")  
+let count2 = document.querySelector("#btn-2").innerHTML
+let count3 = document.querySelector("#btn-3").innerHTML
+let count4 = document.querySelector("#btn-4").innerHTML
+let count5 = document.querySelector("#btn-5").innerHTML
 
 
 btn1Color.addEventListener("click", function(){
-    
     btn1Color.classList.add("clicked")
+    let counting = document.querySelector("#btn-1").innerHTML
+    console.log(counting)
 })
 btn2Color.addEventListener("click", function(){
     btn2Color.classList.add("clicked")
+    let counting = document.querySelector("#btn-2").innerHTML
+    console.log(counting)
 })
 btn3Color.addEventListener("click", function(){
     btn3Color.classList.add("clicked")
+    let counting = " " 
+    counting = count3.value
 })
 btn4Color.addEventListener("click", function(){
     btn4Color.classList.add("clicked")
+    let counting = " " 
+    counting = count4.value
 })
 btn5Color.addEventListener("click", function(){
     btn5Color.classList.add("clicked")
+    let counting = " " 
+    counting = count5.value
 })
 
 //-------------------Submit
-
-
+let submit = document.querySelector("#submit-btn")
+let teste = document.querySelector(".test")
+submit.addEventListener("click",function(){
+    teste.textContent = counting + " out of 5"
+})
 
 
 // let submit = document.querySelector("#submit-btn")
